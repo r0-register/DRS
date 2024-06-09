ab9d893594a60f155868020623a718a40f6cdf00
r0-register <98020362+r0-register@users.noreply.github.com>
2024-06-09 17:19:30 +0800

    Restore drs-scheduler & scheduler
    (1). 将原本的 drs-scheduler 重命名为论文中的 drs-decision-maker；
    (2). 将原本的 scheduler 重命名为 drs-scheduler；
    (3). 修改 drs-scheduler 的实现方式，之前的实现是直接往K8S调度源码里嵌入(scheduler/generic_scheduler.go，而不是scheduler/framework/plugins/dqn/dqn.go)，改成Scheduling Framework推荐的插件方式；

---
+++ drs-scheduler/pkg/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/kubernetes/cmd/kube-scheduler/app"
+	"k8s.io/kubernetes/pkg/scheduler/framework"
+)
+
+const (
+	// Name is the name of the plugin used in the plugin registry and configurations.
+	Name = "DQNPlugin"
+	// ErrReason returned when node name doesn't match.
+	ErrReason = "This node is not the result given by the RL scheduler"
+)
+
+type DQNPlugin struct {
+	handle framework.Handle
+}
+
+var _ framework.FilterPlugin = DQNPlugin{}
+
+func (dp DQNPlugin) Name() string {
+	return Name
+}
+
+func (dp DQNPlugin) Filter(ctx context.Context, cycleState *framework.CycleState, pod *v1.Pod, nodeInfo *framework.NodeInfo) *framework.Status {
+	fmt.Printf("[INFO] Filter pod: %v, node: %v\n", pod.Name, nodeInfo.Node().Name)
+	node := nodeInfo.Node()
+	if node == nil {
+		return framework.NewStatus(framework.Error, "node not found")
+	}
+
+	// get result from rl algorithm
+	schedulerUrl := "http://172.31.234.111:1234/choose"
+	urlValues := url.Values{}
+	urlValues.Add("podname", pod.Name)
+	resp, err := http.PostForm(schedulerUrl, urlValues)
+	if err != nil {
+		fmt.Printf("[ERROR] Get choose from %v failed\n", schedulerUrl)
+		fmt.Printf("[INFO] RL scheduling failed, all node will pass the filter\n\n")
+		return framework.NewStatus(framework.Success, "")
+	}
+	body, _ := io.ReadAll(resp.Body)
+	choose := string(body)
+
+	if node.Name == choose {
+		fmt.Printf("[INFO] Filter pod successfully: %v, node: %v\n\n", pod.Name, nodeInfo.Node().Name)
+		return framework.NewStatus(framework.Success, "")
+	} else {
+		fmt.Printf("[ERROR] Filter pod failed: %v, node: %v\n\n", pod.Name, nodeInfo.Node().Name)
+		return framework.NewStatus(framework.Error, fmt.Sprintf("%s: %s", ErrReason, choose))
+	}
+}
+
+func New(_ runtime.Object, h framework.Handle) (framework.Plugin, error) {
+	return &DQNPlugin{handle: h}, nil
+}
+
+func main() {
+	fmt.Printf("[INFO] Start DQN plugin\n")
+
+	// create a new scheduler
+	command := app.NewSchedulerCommand(
+		app.WithPlugin(Name, New),
+	)
+
+	// run the scheduler
+	if err := command.Execute(); err != nil {
+		fmt.Printf("[ERROR] Run scheduler failed: %v\n", err)
+		os.Exit(1)
+	}
+}
